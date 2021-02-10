3455a3e69466650731b0cf2ac75332b6d17a5bce
luasenvy <luas.envy@gmail.com>
2021-02-10 14:08:16 +0900

    merge

--- src/interfaces/Middleware.js
+++ src/interfaces/Middleware.js
@@ -2,7 +2,6 @@ class Middleware {
   constructor() {
     this.isDev = process.env.NODE_ENV !== 'production'
     this.$rapidfire = null
-    this.pattern = null
     this.pipelines = []
     this.order = 0
   }
