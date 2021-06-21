0610d890892c3065c2b91a1c6f218f1c3f28a5d3
luasenvy <luas.envy@gmail.com>
2021-06-21 15:01:50 +0900

    remove Multer

--- src/interfaces/Controller.js
+++ src/interfaces/Controller.js
@@ -1,13 +1,8 @@
-const path = require('path')
-
-const Multer = require('multer')
-
 /** @interface */
 class Controller {
   /** Create Controller */
   constructor() {
     this._$rapidfire = null
-    this._multer = Multer({ dest: path.join('/tmp', process.env.npm_package_name || '/rapid-fire-uploads/') })
   }
 
   /**
@@ -19,17 +14,6 @@ class Controller {
    */
   get $rapidfire() {
     return this._$rapidfire
-  }
-
-  /**
-   * {@link https://github.com/expressjs/multer Multer} Instance
-   *
-   * @instance
-   * @readonly
-   * @type {Multer}
-   */
-  get multer() {
-    return this._multer
   }
 
   async init() {}
