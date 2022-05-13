1612b266cd9dd5901aeaf5a3ae50ae8026f368f0
luasenvy <luas.envy@gmail.com>
2022-05-13 10:05:53 +0900

    code clean and update docs

--- src/interfaces/index.js
+++ src/interfaces/index.js
@@ -2,6 +2,5 @@ const Controller = require('./Controller')
 const Middleware = require('./Middleware')
 const ServiceLoader = require('./ServiceLoader')
 const Service = require('./Service')
-const Scheme = require('./Scheme')
 
-module.exports = { Controller, Middleware, ServiceLoader, Service, Scheme }
+module.exports = { Controller, Middleware, ServiceLoader, Service }
